2837c13bcf115364408ea856b0ad6356ac44f766
EmilioRosso <7zen@ukr.net>
2020-07-05 15:06:16 +0300

    Updates

---
+++ precache-manifest.49e171f78716ffa6f08b6f070e3eb3d0.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "83e44e799bc7d5371fd1566b0fc10b10",
+    "url": "/abz_test/index.html"
+  },
+  {
+    "revision": "9d205cdbda3197db7549",
+    "url": "/abz_test/static/css/main.4f3b23bc.chunk.css"
+  },
+  {
+    "revision": "89c70360e6daf28e6c89",
+    "url": "/abz_test/static/js/2.8092bb86.chunk.js"
+  },
+  {
+    "revision": "77e7f328dacc981fca681ee4ae94d669",
+    "url": "/abz_test/static/js/2.8092bb86.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9d205cdbda3197db7549",
+    "url": "/abz_test/static/js/main.0e5ede27.chunk.js"
+  },
+  {
+    "revision": "8a5e62630edc5ab08582",
+    "url": "/abz_test/static/js/runtime-main.184aee8d.js"
+  },
+  {
+    "revision": "aa71c0ae5c3f306464c3816972d4726f",
+    "url": "/abz_test/static/media/banner-photo-desktop.aa71c0ae.jpg"
+  },
+  {
+    "revision": "b8204376a6a3c99d43af5c3c87416ecb",
+    "url": "/abz_test/static/media/banner-photo-desktop@2x.b8204376.jpg"
+  },
+  {
+    "revision": "18ccb65ee59f3939bba04b39090a6398",
+    "url": "/abz_test/static/media/banner-photo-mobile.18ccb65e.jpg"
+  },
+  {
+    "revision": "a2c7963fb187e2b48c7886e7e9170cfc",
+    "url": "/abz_test/static/media/banner-photo-mobile@2x.a2c7963f.jpg"
+  },
+  {
+    "revision": "92289af4adc2d7f2e860a6cbb3171a10",
+    "url": "/abz_test/static/media/banner-photo-tablet.92289af4.jpg"
+  },
+  {
+    "revision": "12b03ba402c8cdb598d2fbc9b091030b",
+    "url": "/abz_test/static/media/banner-photo-tablet@2x.12b03ba4.jpg"
+  },
+  {
+    "revision": "2491afe95f7cac40010a141ae5c5fc77",
+    "url": "/abz_test/static/media/open-sans-v17-latin_cyrillic-regular.2491afe9.woff"
+  },
+  {
+    "revision": "5fffd42d7079f1a96aa8dbf6aa2a82b0",
+    "url": "/abz_test/static/media/open-sans-v17-latin_cyrillic-regular.5fffd42d.ttf"
+  },
+  {
+    "revision": "65a957182ddbd935f71058b102385304",
+    "url": "/abz_test/static/media/open-sans-v17-latin_cyrillic-regular.65a95718.eot"
+  },
+  {
+    "revision": "7aab4c13671282c90669eb6a10357e41",
+    "url": "/abz_test/static/media/open-sans-v17-latin_cyrillic-regular.7aab4c13.svg"
+  },
+  {
+    "revision": "aeb4b1bfc96bce2be7d95d9917d8cccb",
+    "url": "/abz_test/static/media/open-sans-v17-latin_cyrillic-regular.aeb4b1bf.woff2"
+  }
+]);
